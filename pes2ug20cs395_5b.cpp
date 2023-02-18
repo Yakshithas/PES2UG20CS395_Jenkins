9a48225cf4a5cdd0b160fee35b6ac49c147ca7a1
Yakshithas <118300382+Yakshithas@users.noreply.github.com>
2023-02-18 17:40:19 +0530

    Add files via upload

---
+++ pes2ug20cs395_5b.cpp
@@ -0,0 +1,12 @@
+//File: pes2ug20cs395_5b.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, happy sivaratri\n";
+  cout << "Hello, 2023\n";
+  cout << "PES2UG20CS395\n";
+  cout << "Successful Pipeline\n";
+  cout << "Bye\n";
+  return 0;
+}
